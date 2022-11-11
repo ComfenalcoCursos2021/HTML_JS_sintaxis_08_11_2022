a15102cbf08093c8c082cc6f24bced036e84dee3
ComfenalcoCursos2021 <educacioncontinua04@unc.edu.co>
2022-11-10 19:51:00 -0500

    Mostrar datos y solicitar

---
+++ main.js
@@ -0,0 +1,9 @@
+addEventListener(`DOMContentLoaded`, (e)=>{
+    console.log("Hola como estas mensaje consola :)");
+    alert("Hola como estas soy la vetana :P");
+    prompt(`Usuario ingrese su nombre`, "Ejemplo 'Pepito perez'");
+    confirm("Usuario estas entendien JavaScript?");
+    document.querySelector("body").innerHTML = "Hola como este es un mensaje del JavaScript :*";
+})
+
+
